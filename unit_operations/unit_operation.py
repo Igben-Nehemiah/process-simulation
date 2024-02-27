0c38543d29126c6026f906e785a9607644e9df6d
Igben Nehemiah <igbennehemiah@gmail.com>
2024-02-27 23:06:34 +0000

    created a base class to represent a generic unit operation

---
+++ unit_operations/unit_operation.py
@@ -0,0 +1,14 @@
+"""
+Created by ...
+"""
+from abc import ABC
+
+
+class UnitOperation(ABC):
+    """
+    This is a simple abstraction for a unit operation.
+
+    """
+
+    def __init__(self, name: str) -> None:
+        pass
