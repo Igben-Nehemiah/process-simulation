e93e11656457a87b738cadf339a1b45dc7b91b40
Igben Nehemiah <igbennehemiah@gmail.com>
2024-02-28 01:00:39 +0000

    created a simple vessel class

---
+++ unit_operations/vessels/simple_vessel.py
@@ -0,0 +1,19 @@
+"""
+Created by
+"""
+
+from unit_operations.stream.stream import Stream
+from unit_operations.unit_operation import UnitOperation
+
+
+class SimpleVessel(UnitOperation):
+    """
+    A simple abstraction of a process vessel
+    """
+
+    def __init__(self, name: str,
+                 input_streams: list[Stream],
+                 output_streams: list[Stream]) -> None:
+        super().__init__(name)
+        self.input_streams = input_streams
+        self.output_steams = output_streams
