8ed35cdb4c97b312335a57ab7664d103f81dd7bb
Igben Nehemiah <igbennehemiah@gmail.com>
2024-03-03 10:04:10 +0000

    changed solver to scipy's

--- process_simulator/src/unit_operations/pipe/friction_factor/colebrook_white.py
+++ process_simulator/src/unit_operations/pipe/friction_factor/colebrook_white.py
@@ -3,9 +3,8 @@ Created by
 """
 
 import numpy as np
-
+from scipy.optimize import newton  # type: ignore
 from ..friction_factor.friction_factor import FrictionFactor
-from ....utils.solvers.newton_raphson import newton_raphson
 
 
 class ColebrookWhite(FrictionFactor):
@@ -19,45 +18,27 @@ class ColebrookWhite(FrictionFactor):
         super().__init__(reynolds_no, rel_roughness)
         self.tol = tol
 
-    def calculate(self) -> float:
+    def calculate_turbulent(self) -> float:
         def f(x):
             return colebrook_white(x, self.rel_roughness, self.reynolds_no)
 
         def f_prime(x):
             return colebrook_white_derivative(x, self.rel_roughness, self.reynolds_no)
 
-        return newton_raphson(f, 0.01, f_prime)[0]
+        return newton(f, 0.01, f_prime)
 
 
-def colebrook_white(f, rel_roughness: float, reynolds_no: float) -> float:
+def colebrook_white(f: float, rel_roughness: float, reynolds_no: float) -> float:
     """
     The Colebrook-White equation.
     """
     return 1 / np.sqrt(f) + 2 * np.log10((rel_roughness) / 3.7 + 2.51 / (reynolds_no * np.sqrt(f)))
 
 
-def colebrook_white_derivative(f, rel_roughness, reynolds_no):
+def colebrook_white_derivative(f: float, rel_roughness, reynolds_no):
     """
     The first derivate of the Colebrook-White equation.
     """
     # Add regime check...
     return - 1/(2*f**(3/2)) - 251/(100*f**(3/2)*reynolds_no*np.log(10) *
                                    ((10*rel_roughness)/37 + 251/(100*f**(1/2)*reynolds_no)))
-
-
-def main():
-    """
-    Simple test... 
-    """
-    # TODO: Move test later
-    roughness = 0.001  # roughness height (m)
-    diameter = 0.1  # diameter of the pipe (m)
-    reynolds_no = 50000  # Reynolds number
-    friction_factor_corr = ColebrookWhite(
-        reynolds_no, roughness/diameter)
-    friction_factor = friction_factor_corr.calculate()
-    print("Approximate Darcy friction factor (Colebrook-White equation):", friction_factor)
-
-
-if __name__ == "__main__":
-    main()
