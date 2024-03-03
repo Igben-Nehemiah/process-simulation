6a14f29749da6bd826bbf6d03b67e95d46ed5632
Igben Nehemiah <igbennehemiah@gmail.com>
2024-03-03 10:05:05 +0000

    added test for transition and laminar regime

--- process_simulator/test/unit_operations/pipe/friction_factor/test_colebrook_white.py
+++ process_simulator/test/unit_operations/pipe/friction_factor/test_colebrook_white.py
@@ -3,7 +3,7 @@ Created by
 """
 import unittest
 
-from .....src.unit_operations.pipe.friction_factor.colebrook_white import ColebrookWhite
+from process_simulator.src.unit_operations.pipe.friction_factor.colebrook_white import ColebrookWhite
 
 
 class ColebrookWhiteTest(unittest.TestCase):
@@ -11,20 +11,45 @@ class ColebrookWhiteTest(unittest.TestCase):
     Simple test cases
     """
 
-    def test_calculate(self) -> None:
+    def setUp(self) -> None:
+        self.roughness = 0.001  # roughness height (m)
+        self.diameter = 0.1  # diameter of the pipe (m)
+
+    def test_calculate_turbulent(self) -> None:
         """
         Test for the calculate method.
         """
-        roughness = 0.001  # roughness height (m)
-        diameter = 0.1  # diameter of the pipe (m)
+
         reynolds_no = 50000  # Reynolds number
         friction_factor_corr = ColebrookWhite(
-            reynolds_no, roughness/diameter)
+            reynolds_no, self.roughness/self.diameter)
         friction_factor = friction_factor_corr.calculate()
 
         self.assertNotEqual(friction_factor, 0.0)
         self.assertGreater(friction_factor, 0)
-        print(friction_factor)
+
+    def test_calculate_transition(self) -> None:
+        """
+        Test for the transition case
+        """
+        reynolds_no = 3500
+        friction_factor_corr = ColebrookWhite(
+            reynolds_no, self.roughness/self.diameter)
+        friction_factor = friction_factor_corr.calculate()
+
+        self.assertGreater(friction_factor, 0)
+        self.assertNotEqual(friction_factor, 64./reynolds_no)
+
+    def test_calculate_laminar(self) -> None:
+        """
+        Test for the laminar case
+        """
+        reynolds_no = 2000
+        friction_factor_corr = ColebrookWhite(
+            reynolds_no, self.roughness/self.diameter)
+        friction_factor = friction_factor_corr.calculate()
+
+        self.assertEqual(friction_factor, 64./reynolds_no)
 
 
 if __name__ == '__main__':
