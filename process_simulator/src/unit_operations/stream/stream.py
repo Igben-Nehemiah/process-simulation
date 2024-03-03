a6484dfa3db69bafb06dfa6250af9ec8abe22b40
Igben Nehemiah <igbennehemiah@gmail.com>
2024-03-03 11:25:24 +0000

    used thermo library for fluid properties

--- process_simulator/src/unit_operations/stream/stream.py
+++ process_simulator/src/unit_operations/stream/stream.py
@@ -2,6 +2,9 @@
 Created by: 
 """
 
+from typing import Literal
+from thermo.chemical import Mixture  # type: ignore
+from thermo.eos_mix import PRMIX  # type: ignore
 from ..stream.component import Component
 from ..unit_operation import UnitOperation
 
@@ -25,16 +28,30 @@ class Stream(UnitOperation):
         if not self._is_composition_sum_ok():
             pass  # throw an exception and suggest normalisation
 
+        self.component_names = list(
+            map(lambda comp: comp.name, composition.keys()))
+        self.component_zs = list(composition.values())
+        self.__mixture = Mixture(self.component_names, self.component_zs,
+                                 T=self.temp, P=self.pressure)
+
+        self.__eos = PRMIX(T=self.__temp,
+                           P=self.__press,
+                           Tcs=self.__mixture.Tcs,
+                           Pcs=self.__mixture.Pcs,
+                           zs=self.__mixture.zs,
+                           omegas=self.__mixture.omegas)
+
     def at(self, temp: float | None = None,
            pressure: float | None = None):
         """
-        Defines new stream at temperature
+        Defines new stream at temperature and pressure
         """
 
         if temp is None:
             temp = self.temp
         if pressure is None:
             pressure = self.pressure
+
         return Stream("", temp, pressure, self.flow_rate, self.composition)
 
     def normalise(self) -> None:
@@ -53,6 +70,48 @@ class Stream(UnitOperation):
         Util method to rename stream
         """
         self._name = name
+
+    @property
+    def phase(self) -> Literal['l', 'g']:
+        """
+        Returns the more stable phase
+        """
+        return self.__eos.more_stable_phase
+
+    @property
+    def dynamic_vis(self) -> float:
+        """
+        Dynamic viscosity
+        """
+        return self.__mixture.mu
+
+    @property
+    def liq_density(self) -> float:
+        """
+        Liquid density
+        """
+        return self.__eos.rho_l
+
+    @property
+    def gas_density(self) -> float:
+        """
+        Liquid density
+        """
+        return self.__eos.rho_g
+
+    @property
+    def mix_critical_temp(self):
+        """
+        Critical temperature of stream.
+        """
+        return self.__eos.Tc
+
+    @property
+    def mix_critical_pressure(self):
+        """
+        Critical pressure of mixture.
+        """
+        return self.__eos.Pc
 
     @property
     def temp(self):
