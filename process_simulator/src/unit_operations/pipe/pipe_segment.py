485141c1c3f1259ab0d3f153ea6a432c288c7711
Igben Nehemiah <igbennehemiah@gmail.com>
2024-03-04 00:08:15 +0000

    updated pressure drop calculations

--- process_simulator/src/unit_operations/pipe/pipe_segment.py
+++ process_simulator/src/unit_operations/pipe/pipe_segment.py
@@ -3,8 +3,8 @@ Created by
 """
 
 from math import pi
-
-from ..stream.stream import Stream
+from thermo import Stream  # type: ignore
+from process_simulator.src.unit_operations.pipe.friction_factor.colebrook_white import ColebrookWhite
 from ..unit_operation import UnitOperation
 
 
@@ -28,9 +28,11 @@ class PipeSegment(UnitOperation):
         self.diameter = diameter
         self.length = length
         self.roughness = roughness
+        self.rel_roughness = roughness/diameter
         self.inlet_stream = inlet_stream
         self.inlet_elevation = inlet_elevation
         self.outlet_elevation = outlet_elevation
+        self.outlet_stream: Stream | None = None
 
     def solve(self):
         """
@@ -38,18 +40,33 @@ class PipeSegment(UnitOperation):
         across the pipe segment. 
         """
         # Calculate pressure drop
-        pressure_drop = 0
-        pressure_out = self.inlet_stream.pressure - pressure_drop
-        return self.inlet_stream.at(temp=self.inlet_stream.temp, pressure=pressure_out)
+        pressure_drop = self.__elevation_pressure_drop() + self.__frictional_pressure_drop()
+        pressure_out = self.inlet_stream.P - pressure_drop
+
+        if pressure_out < 0:
+            raise ArithmeticError("The outlet pressure is less than 0")
+
+        self.outlet_stream = Stream(self.inlet_stream.IDs, zs=self.inlet_stream.zs,
+                                    m=self.inlet_stream.m,
+                                    T=self.inlet_stream.T,
+                                    P=pressure_out)
+
+    @property
+    def pressure_drop(self) -> float | None:
+        """
+        This gives the pressure drop in the pipe
+        """
+        if self.outlet_stream is None:
+            return None
+        return self.inlet_stream.P - self.outlet_stream.P
 
     @property
     def reynolds_no(self) -> float:
         """
         Reynolds number
         """
-        # TODO: check phase
-        return (self.inlet_stream.liq_density * self.velocity *
-                self.diameter)/self.inlet_stream.dynamic_vis
+        return (self.inlet_stream.rho * self.velocity *
+                self.diameter)/self.inlet_stream.mu
 
     @property
     def cross_sectional_area(self) -> float:
@@ -61,4 +78,23 @@ class PipeSegment(UnitOperation):
     @property
     def velocity(self) -> float:
         """Velocity of fluid in pipe"""
-        return self.inlet_stream.flow_rate/self.cross_sectional_area
+        return self.inlet_stream.Q/self.cross_sectional_area
+
+    def __elevation_pressure_drop(self):
+        """
+        This is the pressure drop due to a change in elevation between the inlet 
+        and outlet of the pipe segment.
+        """
+        return (self.outlet_elevation - self.inlet_elevation)*self.inlet_stream.mu
+
+    def __frictional_pressure_drop(self):
+        """
+        This is the major pressure loss due to friction between the moving fluid and 
+        the walls of the pipe. This is calculated using the Darcy-Weisbach equation
+        """
+        f = ColebrookWhite(self.reynolds_no, self.rel_roughness).calculate()
+        # print("Dynamic viscosity: ", self.inlet_stream.mu)
+        # print("Reynolds number: ", self.reynolds_no)
+        # print("Relative roughness: ", self.rel_roughness)
+        # print("Friction factor: ", f)
+        return f*(self.length/self.diameter)*(self.inlet_stream.rho/2)*self.velocity**2
